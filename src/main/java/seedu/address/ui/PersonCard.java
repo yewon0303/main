ed78848063528a09d7eb39a37a7106f7e71a6534
yewon0303 <yewon0303@gmail.com>
2019-10-01 23:57:28 +0800

    Add placeholder for balance to the UI

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -39,6 +39,8 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private Label email;
     @FXML
+    private Label balance;
+    @FXML
     private FlowPane tags;
 
     public PersonCard(Person person, int displayedIndex) {

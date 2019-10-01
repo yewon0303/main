cf8c2b72e91d97acdc9833b8fcf17e959ee41279
yewon0303 <yewon0303@gmail.com>
2019-10-01 23:31:51 +0800

    Tutorial 3 Add SplitCommand

---
+++ src/main/java/seedu/address/logic/commands/SplitCommand.java
@@ -0,0 +1,58 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+
+/**
+ * Splits an amount of money with an existing person in the address book.
+ */
+
+public class SplitCommand extends Command {
+
+    public static final String COMMAND_WORD = "split";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Splits the amount of money " + "with a person. "
+            + "Parameters: AMOUNT (must be a positive integer), INDEX (must be a positive integer)\n "
+            + "split [AMOUNT]" + PREFIX_NAME + "[NAME] \n"
+            + "Example: " + COMMAND_WORD + " split 100" + PREFIX_NAME + "Bob";
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Remark command not implemented yet";
+    public static final String MESSAGE_ARGUMENTS = "Amount: %1$d, Name: %2$s";
+
+    private final int amount;
+    private final String name;
+
+    /**
+     * Constructs SplitCommand
+     * @param amount amount to be split
+     * @param name name of the person to share the amount with
+     */
+    public SplitCommand(int amount, String name) {
+        requireAllNonNull(amount, name);
+
+        this.amount = amount;
+        this.name = name;
+    }
+
+    @Override
+    public CommandResult execute(Model mode) throws CommandException {
+        throw new CommandException(String.format(MESSAGE_ARGUMENTS, amount, name));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        //short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        //instanceof handles nulls
+        if (!(other instanceof SplitCommand)) {
+            return false;
+        }
+
+        //state check
+        SplitCommand e = (SplitCommand) other;
+        return amount == e.amount && name.equals(e.name);
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/SplitCommandParser.java
@@ -0,0 +1,30 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.SplitCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new {@code SplitCommand} object
+ */
+public class SplitCommandParser implements Parser<SplitCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the {@code SplitCommand}
+     * and returns a {@code SplitCommand} object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public SplitCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME);
+
+        int amount = Integer.parseInt(argMultimap.getPreamble());
+        String name = argMultimap.getValue(PREFIX_NAME).orElse("");
+
+        return new SplitCommand(amount, name);
+    }
+
+}
